fedf2ab557913232280c56739c2b8216695b72b3
selvadharani-it19 <selvadharani.it19@bitsathy.ac.in>
2019-08-27 14:15:09 +0530

    first commit

---
+++ mypage.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+</head>
+<body style="font-size:20px;background-color:lightgreen;"><b>
+<h1 style="text-align:center;font-size:30px;">BIO-DATA</h1>
+<br><pre>
+<br>               NAME : SELVADHARANI K<br>
+<br>               DOB  : 09-04-2002<br>
+<br>               AGE  : 17<br>
+<br>      FATHER'S NAME : KUPPURAJ A<br>
+<br>      MOTHER'S NAME : PERIYANAYAKI K<br>
+<br>FATHER'S OCCUPATION : AGRICULTURE<br>
+<br>    KNOWN LANGUAGES : TAMIL,ENGLISH.<br> 
+<br>            ADDRESS : DHARAPURAM,TIRUPPUR DT<br>
+<br>        BLOOD GROUP : A+ve<br>
+<br>           RELIGION : HINDU<br>
+<br>   ACADEMIC RESULTS :<br>
+<br>
+<table border="2" style="font-size:25px;">
+<tr>
+<td>10<sup>nth</td>
+
+<td>11<sup>nth</td>
+<td>12<sup>nth</td>
+</tr>
+<tr>
+<td>94.5%</td>
+<td>84.3%</td>
+<td>88.5%</td>
+</tr>
+</table>
+<br> EXTRA QUALIFICATION: TYPE WRITING IN BOTH ENGLISH AND TAMIL<br>
+
+<br>            HOBBIES :
+<ol>
+<li>READING BOOKS</li>
+<li>litsening to music</li>
+</ol>
+</b>
+</pre>
+</body>
+</html>
